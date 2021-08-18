6eb47675eef7991e5158db55faf9812f3067ddf1
Pani Kumar <panikumar@Panis-MacBook-Pro.local>
2021-08-18 19:27:39 +0530

    Added imports to ease authoring tests. Removed template zip file as it was not needed.

--- src/test/java/ekam/example/api/APIExampleTest.java
+++ src/test/java/ekam/example/api/APIExampleTest.java
@@ -2,6 +2,7 @@ package ekam.example.api;
 
 import com.testvagrant.ekam.testBases.testng.APITest;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 @Test(groups = "api")
 public class APIExampleTest extends APITest {}
--- src/test/java/ekam/example/web/WebExampleTest.java
+++ src/test/java/ekam/example/web/WebExampleTest.java
@@ -2,6 +2,8 @@ package ekam.example.web;
 
 import com.testvagrant.ekam.testBases.testng.WebTest;
 import org.testng.annotations.Test;
+import static com.testvagrant.ekam.commons.LayoutInitiator.*;
+import static org.testng.Assert.*;
 
 @Test(groups = "web")
 public class WebExampleTest extends WebTest {}
