b680dc659dec1e5fdc9d49e0c43f5c59ed0d004f
Pani Kumar E <kumarpani@gmail.com>
2021-08-19 15:31:10 +0530

    Added imports to ease authoring tests. Removed template zip file as i… (#1)
    * Added imports to ease authoring tests. Removed template zip file as it was not needed.
    * Updated template

--- src/test/java/ekam/example/mobile/MobileExampleTest.java
+++ src/test/java/ekam/example/mobile/MobileExampleTest.java
@@ -2,6 +2,8 @@ package ekam.example.mobile;
 
 import com.testvagrant.ekam.testBases.testng.MobileTest;
 import org.testng.annotations.Test;
+import static com.testvagrant.ekam.commons.LayoutInitiator.*;
+import static org.testng.Assert.*;
 
 @Test(groups = "mobile")
 public class MobileExampleTest extends MobileTest {}
